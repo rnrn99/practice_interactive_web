d73f4ec659f221e84ab09bf75be2a3d92adb4b93
giu0213@naver.com <giu0213@naver.com>
2021-05-12 17:17:04 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# practice_interative_web
+
+인터랙티브 웹을 공부하는 기록입니다. 21.05.12~
---
+++ 기초/가위바위보/가위바위보.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    var button = document.querySelector(".startBtn");
+    var text1 = document.getElementsByTagName("h1")[0];
+    var text2 = document.getElementsByTagName("h1")[1];
+    var p = document.getElementsByTagName("p")[0];
+    var arr = ["가위", "바위", "보"];
+
+    button.addEventListener("click", () => {
+       var num1 = Math.round(Math.random() * (arr.length - 1));
+       var num2 = Math.round(Math.random() * (arr.length - 1));
+       text1.innerHTML = arr[num1];
+       text2.innerHTML = arr[num2];
+
+       if(num1 === num2){
+           //비겼을 경우
+           p.innerHTML = "비겼습니다."
+       } else if( num1 % 3 === (num2 + 2) % 3 ){
+           p.innerHTML = "졌습니다."
+       } else {
+           p.innerHTML = "이겼습니다."
+       }
+    })
+}

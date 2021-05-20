78bb479ffcbbaafef43d9c4c805fb3c7522c7c72
giu0213@naver.com <giu0213@naver.com>
2021-05-20 22:32:31 +0900

    tweenMax 활용

--- 패럴랙스스크롤링/parallax.js
+++ 패럴랙스스크롤링/parallax.js
@@ -1,17 +1,53 @@
 window.onload = function() {
     var starBg = document.querySelector('.starBg');
     var title = document.querySelector('.title');
+    var topBtn = document.querySelector('.topBtn');
+    var yOffset = this.scrollY;
 
     window.addEventListener('scroll', () => {
-        var yOffset = this.scrollY;
-        starBg.style.transform = "translateY(" + (-scroll)/3 + "px)";
-        title.style.transform = "translateY(" + scroll/1.7 + "px)";       
+        starBg.style.transform = "translateY(" + (-yOffset)/3 + "px)";
+        title.style.transform = "translateY(" + yOffset/1.7 + "px)";       
     });
 
-    setTimeout(function() {
-        window.scrollTo({
-            top: document.querySelector('bottom').offsetTop,
-            behavior: 'smooth'
-        });       
-    }, 1000);
+    for(var i = 0; i < title.querySelectorAll('div').length; i++){
+        var text = title.querySelectorAll('div')[i];
+
+        TweenMax.from(text, 1, {
+            autoAlpha: 0,
+            delay: Math.random() * 1,
+            ease: Power3.easeInOut
+        });
+    }
+
+    TweenMax.to(window, 2, {
+        scrollTo: {
+            y: ".bottom"
+        },
+        delay: 1.7,
+        ease: Power3.easeInOut
+    });
+
+
+    TweenMax.from(".bottom", 2.5, {
+        scale: 0.7,
+        y: 100,
+        delay: 2.2,
+        ease: Power3.easeInOut
+    });
+
+    TweenMax.to("li", 2.5, {
+        rotateY: 0,
+        delay: 2.2,
+        ease: Power3.easeInOut
+    })
+
+    topBtn.addEventListener('click', () => {
+        TweenMax.to(window, 1.5, {
+            scrollTo: {
+                y: 0,
+                autoKill: true
+            },
+            ease: Power3.easeInOut
+        });
+    });
 }

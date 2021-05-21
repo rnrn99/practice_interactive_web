9ba77df375feb80c0393e5bc583df0fdee41fcdf
giu0213@naver.com <giu0213@naver.com>
2021-05-21 17:33:15 +0900

    card ani no.2 -ing

--- CardTweenmax/Card2.js
+++ CardTweenmax/Card2.js
@@ -1,10 +1,15 @@
-window.onload = function() {
+window.onload = function () {
     var cards = document.querySelectorAll('.cardItem');
     var btn = document.querySelectorAll('button');
     var pageNum = 0;
+    var Width, Height;
 
-    for(var i = 0; i< btn.length; i++){
-        (function(index){
+    window.addEventListener("resize", () => {
+        resize();
+    });
+
+    for (var i = 0; i < btn.length; i++) {
+        (function (index) {
             btn[index].onclick = () => {
                 pageNum = index;
                 cardSetting();
@@ -28,9 +33,51 @@ window.onload = function() {
         });
     });
 
-    TweenMax.set("section", { perspective: 400 });
-    
-    function cardSetting() {
+    TweenMax.set("section", {
+        perspective: 400
+    });
 
+    function resize() {
+        Width = window.innerWidth;
+        Height = window.innerHeight;
+
+        cardSetting();
     }
+
+    function cardSetting() {
+        for (var i = 0; i < btn.length; i++) {
+            btn[i].classList.remove("active");
+        }
+        btn[pageNum].classList.add("active");
+
+        switch (pageNum) {
+            case 0:
+                cards.forEach((item, i) => {
+                    TweenMax.to(item, 1, {
+                        top: Height / 2 - i * 50,
+                        left: Width / 2 + (i * 60 - 200),
+                        rotation: 0,
+                        ease: Power3.easeInOut,
+                        delay: i * 0.2
+                    });
+                });
+            break;
+
+            case 1:
+                cards.forEach((item, i) => {
+                    TweenMax.to(item, 1, {
+                        top: Math.random() * (Height/1.5),
+                        left: Math.random() * (Width/1.5),
+                        rotationX: "random(-60, 60)"
+                    })
+                })
+            break;
+
+            case 2:
+                console.log("2");
+            break;
+        }
+    }
+
+    resize();
 }
